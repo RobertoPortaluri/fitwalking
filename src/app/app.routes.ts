30f64249c638973bff5a63d050bbed42fd05fdc5
Roberto Portaluri <roberto.portaluri@sideagroup.com>
2026-04-16 14:32:07 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
+import { FotoComponent } from './components/foto/foto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'foto', component: FotoComponent }
+];
